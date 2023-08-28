aef92ba3bc12f56963dea4ef6c447c301b12be3b
gazy cool <ndefrutitus22@gmail.com>
2023-08-28 15:32:38 -0400

    created vpc module

--- output.tf
+++ output.tf
@@ -30,7 +30,7 @@ output "public_subnet_az1_id" {
 
 # export the public subnet az2 id
 output "public_subnet_az2_id" {
-  value = aws_subnet.public_subnet_az2.id
+  value =  aws_subnet.public_subnet_az2.id
 }
 
 # export the private app subnet az1 id
