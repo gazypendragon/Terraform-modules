27c7dc9c0c1ed43042d7744ed7f700d371dbf16a
gazy cool <ndefrutitus22@gmail.com>
2023-08-28 21:02:24 -0400

    created vpc module & updated the readme file

--- vpc/main.tf
+++ vpc/main.tf
@@ -86,7 +86,7 @@ resource "aws_subnet" "private_app_subnet_az1" {
 # create private app subnet az2
 resource "aws_subnet" "private_app_subnet_az2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_app_subnet_az1_cidr
+  cidr_block              = var.private_app_subnet_az2_cidr
   availability_zone       = data.aws_availability_zones.available_zones.names[1]
   map_public_ip_on_launch = false
 
