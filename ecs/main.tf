f1dcce97698ac8ba814f039ff5504c83ee4715a3
gazy cool <ndefrutitus22@gmail.com>
2023-09-08 15:18:49 -0400

    created ecs module

--- ecs/main.tf
+++ ecs/main.tf
@@ -67,7 +67,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 # create ecs service
 resource "aws_ecs_service" "ecs_service" {
   name                               = "${var.project_name}-${var.environment}-service"
-  launch_type                        = "FAREGATE"
+  launch_type                        = "FARGATE"
   cluster                            = aws_ecs_cluster.ecs_cluster.id
   task_definition                    = aws_ecs_task_definition.ecs_task_definition.arn
   platform_version                   = "LATEST"
