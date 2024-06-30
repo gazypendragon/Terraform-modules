3fe39e42ef6304bc131fd9da765a9a0f8644079e
choh ndefru <ndefrutitus22@gmail.com>
2024-06-30 07:07:12 -0400

    created ec2-instances module

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -11,7 +11,7 @@ data "aws_ami" "amazon_linux_2" {
 
 # Create EC2 instances
 resource "aws_instance" "ec2_instance" {
-  count                  = var.instance_count
+  count = var.instance_count
 
   ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = var.instance_type
--- ec2-instances/outputs.tf
+++ ec2-instances/outputs.tf
@@ -1,14 +1,14 @@
 #export the IDs of created EC2 instances
 output "instance_ids" {
- value       = aws_instance.ec2_instance[*].id
+  value = aws_instance.ec2_instance[*].id
 }
 
 #export Private IPs of created EC2 instances
 output "private_ips" {
-  value       = aws_instance.ec2_instance[*].private_ip
+  value = aws_instance.ec2_instance[*].private_ip
 }
 
 #export Public IPs of created EC2 instances
 output "public_ips" {
-  value       = aws_instance.ec2_instance[*].public_ip
+  value = aws_instance.ec2_instance[*].public_ip
 }
